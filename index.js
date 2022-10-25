3175597695905962d6b90b02ebc12a2a30db5811
1083698727 <116563063+1083698727@users.noreply.github.com>
2022-10-25 14:37:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     var name = document.getElementById("text1").value;
     var nameLength = name.toString.length;
     if (nameLength == 0) {
-        document.getElementById("a1").innerHTML = nameLength;
+        document.getElementById("a1").innerHTML = "快点，必须欧打一个人！";
     }
     else {
         if (nameLength > 8) {
