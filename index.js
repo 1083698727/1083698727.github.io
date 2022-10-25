14a29ba15e7684a966ccb40518160f9ccc8ef5df
1083698727 <116563063+1083698727@users.noreply.github.com>
2022-10-25 11:20:20 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+﻿function confirm() {
+    var name = document.getElementById("text1").value;
+    var nameLength = name.toString.length;
+    if (nameLength == 0) {
+        document.getElementById("a1").innerHTML = nameLength;
+    }
+    else {
+        if (nameLength > 8) {
+            document.getElementById("a1").innerHTML = "名字太长了懒得打";
+        }
+    }       
+        
+}
